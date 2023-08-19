9ed3a0f4a701da4cad4076d7a061a99a5f510c14
SpOOk <spook.devv@gmail.com>
2023-08-19 12:34:35 -0400

    Added responsive flexboxes

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Anthony</div>;
+  return <div className="hidden text-sm font-semibold md:block">Anthony</div>;
 }
 
 export default Username;
