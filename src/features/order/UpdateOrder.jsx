df9d863b6e76c10ae9800980bee826e9410e24cd
Spook <spook.devv@gmail.com>
2024-03-02 11:09:54 -0500

    Final commit of Fast React Pizza

---
+++ src/features/order/UpdateOrder.jsx
@@ -0,0 +1,22 @@
+import { useFetcher } from 'react-router-dom';
+import Button from '../../ui/Button';
+import { updateOrder } from '../../services/apiRestaurant';
+
+function UpdateOrder({ order }) {
+  const fetcher = useFetcher();
+
+  return (
+    // Use A Form component in order to write/update data without navigating to the page that allows the user to set the priority
+    <fetcher.Form method="PATCH" className="text-right">
+      <Button type="primary">Make Priority</Button>
+    </fetcher.Form>
+  );
+}
+
+export default UpdateOrder;
+
+export async function action({ request, params }) {
+  const data = { priority: true };
+  await updateOrder(params.orderId, data);
+  return null;
+}
